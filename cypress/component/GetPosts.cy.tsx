2c127adfa3ae2e61fb9859d1cf36b59ca0569bba
Arman Abasian <105098083+Arman-Abbasian@users.noreply.github.com>
2024-08-04 09:34:22 +0330

    make the GetPosts spec

---
+++ cypress/component/GetPosts.cy.tsx
@@ -0,0 +1,33 @@
+import GetPosts from '../../src/components/Post/GetPosts';
+
+describe('GetPosts component', () => {
+  it('renders correctly', () => {
+    cy.mount(<GetPosts />);
+    // Add assertions to verify the rendered content
+    cy.contains('h1', 'posts').should('be.visible');
+    cy.get('li').should('have.length.greaterThan', 0);
+  });
+});
+
+  //mock the api
+  describe('GetPosts component', () => {
+    beforeEach(() => {
+      // Mock the API call
+      cy.intercept('GET', 'http://localhost:4000/posts', { fixture: 'posts.js' });
+      cy.mount(<GetPosts />);
+    });
+  
+    it('renders correctly', () => {
+      // Verify the rendered content
+      cy.contains('h1', 'posts').should('be.visible');
+      cy.get('li').should('have.length.greaterThan', 0);
+    });
+  
+    // it('displays post titles', () => {
+    //   // Assuming your fixture has 3 posts
+    //   cy.get('li').should('have.length', 3);
+    //   cy.contains('title 1').should('be.visible');
+    //   cy.contains('title 3').should('be.visible');
+    //   cy.contains('title 4').should('be.visible');
+    // });
+  });
