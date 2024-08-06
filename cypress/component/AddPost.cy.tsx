770ec9ec2e9cbd6358dea197007dacf22d464a46
Arman Abasian <105098083+Arman-Abbasian@users.noreply.github.com>
2024-08-06 09:39:43 +0330

    make the AddPost.cy

--- cypress/component/AddPost.cy.tsx
+++ cypress/component/AddPost.cy.tsx
@@ -1,12 +1,39 @@
-import AddPost from "../../src/components/Post/AddPost";
+import AddPost from '../../src/components/Post/AddPost';
 
-describe('<AddPost />', () => {
-  it('mounts', () => {
-    cy.mount(<AddPost />)
-  });
-  it('renders input fields', () => {
+describe('AddPost component', () => {
+  beforeEach(() => {
+    // Mock the API call for form submission
+    cy.intercept('POST', 'http://localhost:4000/posts', { fixture: 'newPost.json' }).as('addPost');
     cy.mount(<AddPost />);
-    cy.get('#title').should('exist');
-    cy.get('#body').should('exist');
   });
-});
+
+  it('renders correctly', () => {
+    // Check if the form and input fields are rendered
+    cy.get('form').should('be.visible');
+    cy.get('input[name="title"]').should('be.visible');
+    cy.get('input[name="body"]').should('be.visible');
+    cy.get('input[type="submit"]').should('be.visible');
+  });
+
+  it('allows typing into input fields', () => {
+    // Type into the input fields and check their values
+    cy.get('input[name="title"]').type('title 1000').should('have.value', 'title 1000');
+    cy.get('input[name="body"]').type('body 1000').should('have.value', 'body 1000');
+  });
+
+  it('submits the form and resets input fields', () => {
+    // Type into the input fields
+    cy.get('input[name="title"]').type('Test Title');
+    cy.get('input[name="body"]').type('Test Body');
+
+    // Submit the form
+    cy.get('form').submit();
+
+    // Wait for the API call to complete
+    cy.wait('@addPost');
+
+    // Check if the input fields are reset
+    cy.get('input[name="title"]').should('have.value', '');
+    cy.get('input[name="body"]').should('have.value', '');
+  });
+})
--- src/components/Post/AddPost.cy.tsx
+++
@@ -1,8 +0,0 @@
-import AddPost from './AddPost'
-
-describe('<AddPost />', () => {
-  it('renders', () => {
-    // see: https://on.cypress.io/mounting-react
-    cy.mount(<AddPost />)
-  })
-})
