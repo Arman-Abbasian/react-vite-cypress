9683ca0a7838b5ba19db7570aaa5b6ba843c8efc
Arman Abasian <105098083+Arman-Abbasian@users.noreply.github.com>
2024-08-07 09:18:45 +0330

    some changes

--- cypress/component/AddPost.cy.tsx
+++ cypress/component/AddPost.cy.tsx
@@ -15,23 +15,14 @@ describe('AddPost component', () => {
     cy.get('input[type="submit"]').should('be.visible');
   });
 
-  it('allows typing into input fields', () => {
-    // Type into the input fields and check their values
-    cy.get('input[name="title"]').type('title 1000').should('have.value', 'title 1000');
-    cy.get('input[name="body"]').type('body 1000').should('have.value', 'body 1000');
-  });
-
   it('submits the form and resets input fields', () => {
     // Type into the input fields
     cy.get('input[name="title"]').type('Test Title');
     cy.get('input[name="body"]').type('Test Body');
-
     // Submit the form
     cy.get('form').submit();
-
     // Wait for the API call to complete
     cy.wait('@addPost');
-
     // Check if the input fields are reset
     cy.get('input[name="title"]').should('have.value', '');
     cy.get('input[name="body"]').should('have.value', '');
